9755017cb61e2728006928cf82b9effd09865f67
taka292 <66032836+taka292@users.noreply.github.com>
2025-12-31 18:48:59 +0900

    add: カラーパレット機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   # 投稿機能
   resources :posts
 
+  # カラーパレット機能
+  get "palette", to: "palette#index", as: :palette
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
